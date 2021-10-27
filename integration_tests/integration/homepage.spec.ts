526fd238542ea549c49c75c65935035d83f7ddf6
richardpopple <richard.popple@digital.justice.gov.uk>
2021-10-27 09:24:08 +0100

    NN-3718: add boiler platefeatures footer (#11)
    * NN-3718 adding the footer and feedback
    
    * NN-3718 tidy up around tests
    
    * NN-3718 fixing the terms and conditions url and the cypress tests
    
    * NN-3718 fixing linting issue

---
+++ integration_tests/integration/homepage.spec.ts
@@ -0,0 +1,17 @@
+import Homepage from '../pages/homepage'
+import Page from '../pages/page'
+
+context('Homepage', () => {
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubSignIn')
+    cy.task('stubAuthUser')
+    cy.signIn()
+  })
+
+  it('should see the feedback banner', () => {
+    cy.visit(`/`)
+    const homepage: Homepage = Page.verifyOnPage(Homepage)
+    homepage.feedbackBanner().should('exist')
+  })
+})
