07918f92ed82d23a94ae579255830a0853e2feae
Richard Popple <richard.popple@digital.justice.gov.uk>
2021-10-28 16:31:43 +0100

    NN-3655 does this fix the tests

--- server/data/prisonerSearchClient.test.ts
+++ server/data/prisonerSearchClient.test.ts
@@ -16,78 +16,79 @@ describe('prisonerSearchClient', () => {
   afterEach(() => {
     nock.cleanAll()
   })
+  describe('qqRP', () => {
+    it('search by prisoner identifier', async () => {
+      const results: unknown[] = []
+      fakePrisonerSearchApi
+        .post(
+          `/prisoner-search/match-prisoners`,
+          '{"includeAliases":false,"prisonerIdentifier":"A1234AA","prisonIds":["PR1","PR2"]}'
+        )
+        .matchHeader('authorization', `Bearer ${token}`)
+        .reply(200, results)
 
-  it('search by prisoner identifier', async () => {
-    const results: unknown[] = []
-    fakePrisonerSearchApi
-      .post(
-        `/prisoner-search/match-prisoners`,
-        '{"includeAliases":false,"prisonerIdentifier":"A1234AA","prisonIds":["PR1","PR2"]}'
-      )
-      .matchHeader('authorization', `Bearer ${token}`)
-      .reply(200, results)
+      const output = await client.search({ prisonerIdentifier: 'A1234AA', prisonIds: ['PR1', 'PR2'] })
 
-    const output = await client.search({ prisonerIdentifier: 'A1234AA', prisonIds: ['PR1', 'PR2'] })
+      expect(output).toEqual(results)
+    })
 
-    expect(output).toEqual(results)
-  })
+    it('search by prisoner name', async () => {
+      const results: unknown[] = []
+      fakePrisonerSearchApi
+        .post(
+          `/prisoner-search/match-prisoners`,
+          '{"includeAliases":false,"firstName":"John","lastName":"Smith","prisonIds":["PR1","PR2"]}'
+        )
+        .matchHeader('authorization', `Bearer ${token}`)
+        .reply(200, results)
 
-  it('search by prisoner name', async () => {
-    const results: unknown[] = []
-    fakePrisonerSearchApi
-      .post(
-        `/prisoner-search/match-prisoners`,
-        '{"includeAliases":false,"firstName":"John","lastName":"Smith","prisonIds":["PR1","PR2"]}'
-      )
-      .matchHeader('authorization', `Bearer ${token}`)
-      .reply(200, results)
+      const output = await client.search({ firstName: 'John', lastName: 'Smith', prisonIds: ['PR1', 'PR2'] })
 
-    const output = await client.search({ firstName: 'John', lastName: 'Smith', prisonIds: ['PR1', 'PR2'] })
+      expect(output).toEqual(results)
+    })
 
-    expect(output).toEqual(results)
-  })
+    it('search including aliases', async () => {
+      const results: unknown[] = []
+      fakePrisonerSearchApi
+        .post(`/prisoner-search/match-prisoners`, '{"includeAliases":true,"prisonerIdentifier":"A1234AA"}')
+        .matchHeader('authorization', `Bearer ${token}`)
+        .reply(200, results)
 
-  it('search including aliases', async () => {
-    const results: unknown[] = []
-    fakePrisonerSearchApi
-      .post(`/prisoner-search/match-prisoners`, '{"includeAliases":true,"prisonerIdentifier":"A1234AA"}')
-      .matchHeader('authorization', `Bearer ${token}`)
-      .reply(200, results)
+      const output = await client.search({ includeAliases: true, prisonerIdentifier: 'A1234AA' })
 
-    const output = await client.search({ includeAliases: true, prisonerIdentifier: 'A1234AA' })
+      expect(output).toEqual(results)
+    })
 
-    expect(output).toEqual(results)
-  })
+    it('parses response correctly', async () => {
+      const response = [
+        {
+          something: 'to ignore',
+          prisonerNumber: 'A1234AA',
+          firstName: 'John',
+          lastName: 'Smith',
+          prisonId: 'LEI',
+          prisonName: 'HMP Leeds',
+          cellLocation: 'LEI-1-2',
+        },
+      ]
 
-  it('parses response correctly', async () => {
-    const response = [
-      {
-        something: 'to ignore',
-        prisonerNumber: 'A1234AA',
-        firstName: 'John',
-        lastName: 'Smith',
-        prisonId: 'LEI',
-        prisonName: 'HMP Leeds',
-        cellLocation: 'LEI-1-2',
-      },
-    ]
+      fakePrisonerSearchApi
+        .post(`/prisoner-search/match-prisoners`, '{"includeAliases":false,"prisonerIdentifier":"A1234AA"}')
+        .matchHeader('authorization', `Bearer ${token}`)
+        .reply(200, response)
 
-    fakePrisonerSearchApi
-      .post(`/prisoner-search/match-prisoners`, '{"includeAliases":false,"prisonerIdentifier":"A1234AA"}')
-      .matchHeader('authorization', `Bearer ${token}`)
-      .reply(200, response)
+      const output = await client.search({ prisonerIdentifier: 'A1234AA' })
 
-    const output = await client.search({ prisonerIdentifier: 'A1234AA' })
-
-    expect(output).toEqual([
-      {
-        prisonerNumber: 'A1234AA',
-        firstName: 'John',
-        lastName: 'Smith',
-        prisonId: 'LEI',
-        prisonName: 'HMP Leeds',
-        cellLocation: 'LEI-1-2',
-      },
-    ])
+      expect(output).toEqual([
+        {
+          prisonerNumber: 'A1234AA',
+          firstName: 'John',
+          lastName: 'Smith',
+          prisonId: 'LEI',
+          prisonName: 'HMP Leeds',
+          cellLocation: 'LEI-1-2',
+        },
+      ])
+    })
   })
 })
