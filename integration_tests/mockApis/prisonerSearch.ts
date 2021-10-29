ef1c5fb36fc700dd9f72d21fb4eb31b5828341d2
Richard Popple <richard.popple@digital.justice.gov.uk>
2021-10-29 11:16:39 +0100

    NN-3655 fixing linting

--- integration_tests/mockApis/prisonerSearch.ts
+++ integration_tests/mockApis/prisonerSearch.ts
@@ -1,6 +1,7 @@
+import { SuperAgentRequest } from 'superagent'
 import { stubFor } from './wiremock'
 
-const stubPing = (status = 200) =>
+const stubPing = (status = 200): SuperAgentRequest =>
   stubFor({
     request: {
       method: 'GET',
