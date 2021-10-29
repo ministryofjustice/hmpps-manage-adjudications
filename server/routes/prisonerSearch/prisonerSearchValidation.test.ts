04139fd6f7c6799bf9c2dfe66647ffe8415c7b0e
Richard Popple <richard.popple@digital.justice.gov.uk>
2021-10-29 14:51:37 +0100

    NN-3655 added tests

---
+++ server/routes/prisonerSearch/prisonerSearchValidation.test.ts
@@ -0,0 +1,16 @@
+import validateForm from './prisonerSearchValidation'
+
+describe('validateForm', () => {
+  describe('searchTerm', () => {
+    it('valid', () => {
+      expect(validateForm({ searchTerm: 'Smith' })).toBeNull()
+    })
+
+    it('invalid', () => {
+      expect(validateForm({ searchTerm: '' })).toStrictEqual({
+        href: '#searchTerm',
+        text: 'Enter a name or prison number',
+      })
+    })
+  })
+})
