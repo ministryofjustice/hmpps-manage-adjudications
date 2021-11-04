8326f16e42efdcf90e161de989d76b696632fa6b
sp-watson <steve.watson@digital,justice.gov.uk>
2021-11-04 15:28:27 +0000

    NN-3668: Another attempt to fix linting errors

--- server/services/reportedAdjudicationsService.ts
+++ server/services/reportedAdjudicationsService.ts
@@ -5,6 +5,7 @@ import { ConfirmedOnReportData } from '../data/ConfirmedOnReportData'
 export default class ReportedAdjudicationService {
   async getReportedAdjudication(adjudicationNumber: number, user: User): Promise<ConfirmedOnReportData> {
     // const adjudicationData = await new ManageAdjudicationsClient(user.token).getReportedAdjudication(adjudicationNumber)
+    /* eslint-disable no-console */
     console.log(`TODO - remove ${adjudicationNumber}, ${user}`)
     return {
       reportExpirationDateTime: '2021-10-25T09:03:11',
