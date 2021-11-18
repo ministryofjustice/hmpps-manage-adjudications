e03f01fd67f1c83da0a32943d8f1645f5acdb1b2
Natalie Clamp <natalie.clamp@digital.justice.gov.uk>
2021-11-18 16:58:09 +0000

    NN-3670: Add hash to CSP

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -13,7 +13,13 @@ export default function setUpWebSecurity(): Router {
         directives: {
           defaultSrc: ["'self'"],
           // Hash allows inline script pulled in from https://github.com/alphagov/govuk-frontend/blob/master/src/govuk/template.njk
-          scriptSrc: ["'self'", 'code.jquery.com', "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='"],
+          scriptSrc: [
+            "'self'",
+            'code.jquery.com',
+            "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='",
+            "'sha256-loNEcvOiwPZFEqTNJYE+/sdSv0X/6zDcCFyxAG+oGiw='",
+            "'unsafe-hashes'",
+          ],
           styleSrc: ["'self'", 'code.jquery.com', "'unsafe-inline'"],
           fontSrc: ["'self'"],
           imgSrc: ["'self'", 'code.jquery.com'],
