40ba1249969ffe4fb7e6e604e07865fd8a14cc5e
Nat Clamp <84783598+natclamp-moj@users.noreply.github.com>
2021-12-09 10:26:53 +0000

    NN-3852: revert removal print report (#87)
    * NN-3852: Add print-report page files back in
    
    * NN-3852: Alter print-report page and connect up

---
+++ integration_tests/integration/printReport.spec.ts
@@ -0,0 +1,96 @@
+import Page from '../pages/page'
+import PrintReport from '../pages/printReport'
+
+context('Print a copy of this report', () => {
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubSignIn')
+    cy.task('stubAuthUser')
+    cy.task('stubGetPrisonerDetails', {
+      prisonerNumber: 'G6415GD',
+      response: {
+        offenderNo: 'G6415GD',
+        firstName: 'JOHN',
+        lastName: 'SMITH',
+        assignedLivingUnit: { description: '1-2-015', agencyName: 'Moorland (HMPYOI)' },
+        categoryCode: 'C',
+        language: 'French',
+        alerts: [
+          { alertType: 'T', alertCode: 'TCPA' },
+          { alertType: 'X', alertCode: 'XCU' },
+        ],
+      },
+    })
+    cy.task('stubGetReportedAdjudication', {
+      id: 1524242,
+      response: {
+        reportedAdjudication: {
+          adjudicationNumber: 3,
+          prisonerNumber: 'G6415GD',
+          bookingId: 123,
+          dateTimeReportExpires: '2020-12-08T10:00:00',
+          incidentDetails: {
+            locationId: 2,
+            dateTimeOfIncident: '2020-12-06T10:00:00',
+          },
+          incidentStatement: {
+            statement: 'test',
+          },
+        },
+      },
+    })
+    cy.task('stubGetSecondaryLanguages', {
+      bookingId: 123,
+      response: [
+        {
+          code: 'SPA',
+          description: 'Spanish',
+          canRead: true,
+          canWrite: false,
+          canSpeak: true,
+        },
+        {
+          code: 'GER',
+          description: 'German',
+          canRead: true,
+          canWrite: false,
+          canSpeak: false,
+        },
+      ],
+    })
+    cy.task('stubGetNeurodiversities', {
+      prisonerNumber: 'G6415GD',
+      response: [
+        {
+          prn: 'G6415GD',
+          establishmentId: 'MDI',
+          uln: '1234123412',
+          primaryLDDAndHealthProblem: 'Speech, language and communication needs',
+          additionalLDDAndHealthProblems: ['Hearing impairment', 'Other difficulty'],
+        },
+      ],
+    })
+
+    cy.signIn()
+  })
+
+  it('should contain the required page elements', () => {
+    cy.visit(`/print-report/1524242`)
+    Page.verifyOnPage(PrintReport)
+    cy.contains('John Smith must be given a copy of this report by 10:00 on Tuesday 8 December')
+    cy.contains('John Smith’s preferred language is:')
+    cy.contains('French')
+    cy.contains('They have other languages of:')
+    cy.contains('Spanish')
+    cy.contains('German')
+  })
+
+  it('should redirect the user to the referrer on finish', () => {
+    cy.visit(`/print-report/1524242?referrer=/prisoner-report/G6415GD/1524242/report`)
+    const printReportPage = Page.verifyOnPage(PrintReport)
+    printReportPage.exitButton().click()
+    cy.location().should(loc => {
+      expect(loc.pathname).to.eq('/prisoner-report/G6415GD/1524242/report')
+    })
+  })
+})
---
+++ server/routes/printReport/printReport.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express'
+
+import ReportedAdjudicationsService from '../../services/reportedAdjudicationsService'
+import { formatName, formatTimestampToDate, formatTimestampToTime } from '../../utils/utils'
+
+export default class PrintReportRoutes {
+  constructor(private readonly reportedAdjudicationsService: ReportedAdjudicationsService) {}
+
+  private renderView = async (req: Request, res: Response): Promise<void> => {
+    const { adjudicationNumber } = req.params
+    const { referrer } = req.query
+    const { user } = res.locals
+
+    const adjudicationNumberValue: number = parseInt(adjudicationNumber as string, 10)
+    if (Number.isNaN(adjudicationNumberValue)) {
+      throw new Error('No adjudication number provided')
+    }
+
+    const adjudicationDetails = await this.reportedAdjudicationsService.getReportedAdjudication(
+      adjudicationNumberValue,
+      user
+    )
+
+    return res.render(`pages/printReport`, {
+      adjudicationNumber: adjudicationNumberValue,
+      expirationTime: formatTimestampToTime(adjudicationDetails.reportExpirationDateTime),
+      expirationDay: formatTimestampToDate(adjudicationDetails.reportExpirationDateTime, 'dddd D MMMM'),
+      prisonerFirstAndLastName: formatName(adjudicationDetails.prisonerFirstName, adjudicationDetails.prisonerLastName),
+      showPrisonerPreferredLanguage: adjudicationDetails.prisonerPreferredNonEnglishLanguage != null,
+      prisonerPreferredLanguage: adjudicationDetails.prisonerPreferredNonEnglishLanguage,
+      showPrisonerOtherLanguages: adjudicationDetails.prisonerOtherLanguages?.length > 0,
+      prisonerOtherLanguages: adjudicationDetails.prisonerOtherLanguages,
+      showPrisonerNeurodiversities: adjudicationDetails.prisonerNeurodiversities?.length > 0,
+      prisonerNeurodiversities: adjudicationDetails.prisonerNeurodiversities,
+      exitUrl: referrer,
+    })
+  }
+
+  view = async (req: Request, res: Response): Promise<void> => this.renderView(req, res)
+}
