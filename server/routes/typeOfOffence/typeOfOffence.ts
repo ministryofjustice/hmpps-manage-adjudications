7c3b7d8ac8e77fda2f9fff1adb9ed0f5adb87176
Natalie Clamp <natalie.clamp@digital.justice.gov.uk>
2022-01-06 17:03:52 +0000

    NN-3894: Add simple tests to check rendering

--- server/routes/typeOfOffence/typeOfOffence.ts
+++ server/routes/typeOfOffence/typeOfOffence.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express'
 export default class OffenceDetailsRoutes {
   private renderView = async (req: Request, res: Response): Promise<void> => {
     const { prisonerNumber, id } = req.params
-    return res.render(`pages/offenceDetails`, {
+    return res.render(`pages/typeOfOffence`, {
       prisonerNumber,
       id,
     })
