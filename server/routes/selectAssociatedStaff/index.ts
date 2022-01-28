b9be8691dc561e5ed158eced8f9d27ac8a5c6691
Natalie Clamp <natalie.clamp@digital.justice.gov.uk>
2022-01-28 16:19:54 +0000

    NN-3866: Staff member page refactor

--- server/routes/selectAssociatedStaff/index.ts
+++ server/routes/selectAssociatedStaff/index.ts
@@ -4,11 +4,18 @@ import asyncMiddleware from '../../middleware/asyncMiddleware'
 import SelectAssociatedStaffRoutes from './selectAssociatedStaff'
 
 import UserService from '../../services/userService'
+import PlaceOnReportService from '../../services/placeOnReportService'
 
-export default function selectAssociatedStaffRoutes({ userService }: { userService: UserService }): Router {
+export default function selectAssociatedStaffRoutes({
+  userService,
+  placeOnReportService,
+}: {
+  userService: UserService
+  placeOnReportService: PlaceOnReportService
+}): Router {
   const router = express.Router()
 
-  const selectAssociatedStaff = new SelectAssociatedStaffRoutes(userService)
+  const selectAssociatedStaff = new SelectAssociatedStaffRoutes(userService, placeOnReportService)
 
   const get = (path: string, handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
   const post = (path: string, handler: RequestHandler) => router.post(path, asyncMiddleware(handler))
