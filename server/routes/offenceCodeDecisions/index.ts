a1cc20c14e76132bf68eddcc058c940eb8520bd4
Richard Popple <richard.popple@digital.justice.gov.uk>
2022-02-07 11:51:04 +0000

    NN-3935 only using urls that exist

--- server/routes/offenceCodeDecisions/index.ts
+++ server/routes/offenceCodeDecisions/index.ts
@@ -2,6 +2,7 @@ import express, { RequestHandler, Router } from 'express'
 import asyncMiddleware from '../../middleware/asyncMiddleware'
 import OffenceCodeDecisionsRoutes from './offenceCodeDecisions'
 import PlaceOnReportService from '../../services/placeOnReportService'
+import decisionTrees from '../../offenceCodeDecisions/DecisionTrees'
 
 export default function offenceCodeDecisionsRoutes({
   placeOnReportService,
@@ -12,7 +13,11 @@ export default function offenceCodeDecisionsRoutes({
   const offenceCodeDecisions = new OffenceCodeDecisionsRoutes(placeOnReportService)
   const get = (path: string, handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
   const post = (path: string, handler: RequestHandler) => router.post(path, asyncMiddleware(handler))
-  get('/:adjudicationNumber/:incidentRole/*', offenceCodeDecisions.view)
-  post('/:adjudicationNumber/:incidentRole/*', offenceCodeDecisions.submit)
+  decisionTrees.forEach(decisionTree =>
+    decisionTree.allUrls().forEach(url => {
+      get(`/:adjudicationNumber/:incidentRole/${url}`, offenceCodeDecisions.view)
+      post(`/:adjudicationNumber/:incidentRole/${url}`, offenceCodeDecisions.submit)
+    })
+  )
   return router
 }
