6950dbac4f5d973c02bd926bd8dd8061e88577e8
Richard Popple <richard.popple@digital.justice.gov.uk>
2022-02-10 16:21:54 +0000

    NN-3935 starting to put validation in plac

--- server/routes/offenceCodeDecisions/offenceCodeDecisionsValidation.ts
+++ server/routes/offenceCodeDecisions/offenceCodeDecisionsValidation.ts
@@ -1,9 +1,12 @@
 import { FormError } from '../../@types/template'
 import { DecisionForm } from './decisionForm'
+import decisionTree from '../../offenceCodeDecisions/DecisionTree'
+import { DecisionType } from '../../offenceCodeDecisions/Decision'
 
 // eslint-disable-next-line no-shadow
 enum ErrorType {
   MISSING_SELECTION = 'MISSING_SELECTION',
+  MISSING_USER_ID = 'MISSING_USER_ID',
 }
 
 const errors: { [key in ErrorType]: FormError } = {
@@ -11,11 +14,27 @@ const errors: { [key in ErrorType]: FormError } = {
     href: '#selectedDecisionId',
     text: 'Please make a choice',
   },
+  MISSING_USER_ID: {
+    href: '#userSearchInput',
+    text: 'TODO',
+  },
 }
 
 export default function validateForm(decisionForm: DecisionForm): FormError | null {
-  if (!decisionForm.selectedDecisionId) {
+  const { selectedDecisionId } = decisionForm
+  if (!selectedDecisionId) {
     return errors.MISSING_SELECTION
+  }
+  switch (decisionTree.findById(selectedDecisionId).getType()) {
+    case DecisionType.STAFF:
+    case DecisionType.PRISONER:
+    case DecisionType.OFFICER:
+      if (!decisionForm.selectedDecisionData.userId) {
+        return errors.MISSING_USER_ID
+      }
+      break
+    default:
+      break
   }
   return null
 }
