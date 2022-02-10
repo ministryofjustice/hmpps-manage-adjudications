2481f16dea15520642cc92da4d3a2c001e9aa2bf
Richard Popple <richard.popple@digital.justice.gov.uk>
2022-02-10 16:53:22 +0000

    NN-3935 more validation

--- server/routes/offenceCodeDecisions/offenceCodeDecisionsValidation.ts
+++ server/routes/offenceCodeDecisions/offenceCodeDecisionsValidation.ts
@@ -3,34 +3,23 @@ import { DecisionForm } from './decisionForm'
 import decisionTree from '../../offenceCodeDecisions/DecisionTree'
 import { DecisionType } from '../../offenceCodeDecisions/Decision'
 
-// eslint-disable-next-line no-shadow
-enum ErrorType {
-  MISSING_SELECTION = 'MISSING_SELECTION',
-  MISSING_USER_ID = 'MISSING_USER_ID',
-}
-
-const errors: { [key in ErrorType]: FormError } = {
-  MISSING_SELECTION: {
-    href: '#selectedDecisionId',
-    text: 'Please make a choice',
-  },
-  MISSING_USER_ID: {
-    href: '#userSearchInput',
-    text: 'TODO',
-  },
-}
-
 export default function validateForm(decisionForm: DecisionForm): FormError | null {
   const { selectedDecisionId } = decisionForm
   if (!selectedDecisionId) {
-    return errors.MISSING_SELECTION
+    return {
+      href: '#selectedDecisionId',
+      text: 'Please make a choice',
+    }
   }
   switch (decisionTree.findById(selectedDecisionId).getType()) {
     case DecisionType.STAFF:
     case DecisionType.PRISONER:
     case DecisionType.OFFICER:
       if (!decisionForm.selectedDecisionData.userId) {
-        return errors.MISSING_USER_ID
+        return {
+          href: `#userSearchInput${selectedDecisionId}`,
+          text: 'Enter their name or user Id',
+        }
       }
       break
     default:
