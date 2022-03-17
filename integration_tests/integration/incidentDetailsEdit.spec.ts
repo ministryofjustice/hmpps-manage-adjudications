d326eb981e8419aab4b39f34ee97725767e00e70
sp-watson <77974320+sp-watson@users.noreply.github.com>
2022-03-17 16:56:18 +0000

    NN-3950: Incident details pages refactor (#165)
    * NN-3950: First commit that compiles
    
    * NN-3950: Got unit tests passing
    
    * NN-3950: Got int tests passing for edit pages
    
    * NN-3950: Lint fixes/exclusions
    
    * NN-3950: Lint exclusions
    
    * NN-3950: Fix original radio selection body param
    
    * NN-3950: Fix search issue and tidy up
    
    * NN-3950: Fix reporter name and minor refactoring
    
    * NN-3950: Minor refactoring
    
    * NN-3950: Convert incident details creation page to use generic page
    
    * NN-3950: Fix originalReporterUsername
    
    * NN-3950: Fix creation redirecturl on search/delete
    
    * NN-3950: Re-enable eslint checks
    
    * NN-3950: More eslint re-enabling
    
    * NN-3950: Final eslint updates
    
    * NN-3950: CircleCi only eslint fixes
    
    Co-authored-by: sp-watson <steve.watson@digital,justice.gov.uk>

--- integration_tests/integration/incidentDetailsEdit.spec.ts
+++ integration_tests/integration/incidentDetailsEdit.spec.ts
@@ -262,7 +262,8 @@ context('Incident details (edit) - statement incomplete', () => {
       expect(loc.pathname).to.eq('/offence-code-selection/34/assisted/1')
     })
   })
-  it('should submit form successfully if all data entered and redirect to /offence-details page - change time', () => {
+  // TODO - To be changed - goes to task list
+  it('should submit form successfully if all data entered and redirect to task list page - change time', () => {
     cy.visit(`/incident-details/G6415GD/34/edit`)
     const incidentDetailsPage: IncidentDetails = Page.verifyOnPage(IncidentDetails)
     incidentDetailsPage.timeInputHours().clear()
@@ -271,16 +272,17 @@ context('Incident details (edit) - statement incomplete', () => {
     incidentDetailsPage.timeInputMinutes().type('00')
     incidentDetailsPage.submitButton().click()
     cy.location().should(loc => {
-      expect(loc.pathname).to.eq('/offence-code-selection/34/incited/1')
+      expect(loc.pathname).to.eq('/place-the-prisoner-on-report/G6415GD/34')
     })
   })
-  it('should submit form successfully if all data entered and redirect to /offence-details page - change location', () => {
+  // TODO - To be changed - goes to task list
+  it('should submit form successfully if all data entered and redirect to task list page - change location', () => {
     cy.visit(`/incident-details/G6415GD/34/edit`)
     const incidentDetailsPage: IncidentDetails = Page.verifyOnPage(IncidentDetails)
     incidentDetailsPage.locationSelector().select('Workshop 2')
     incidentDetailsPage.submitButton().click()
     cy.location().should(loc => {
-      expect(loc.pathname).to.eq('/offence-code-selection/34/incited/1')
+      expect(loc.pathname).to.eq('/place-the-prisoner-on-report/G6415GD/34')
     })
   })
   it('should remember the changed location and time once it comes back to this page from the search page', () => {
@@ -329,7 +331,7 @@ context('Incident details (edit) - statement incomplete', () => {
     incidentDetailsPage.locationSelector().contains('Workshop 2')
     incidentDetailsPage.radioButtons().find('input[value="incited"]').should('be.checked')
   })
-  it('should redirect to the task list page if the user exists the page', () => {
+  it('should redirect to the task list page if the user exits the page', () => {
     cy.visit(`/incident-details/G6415GD/34/edit`)
     const incidentDetailsPage: IncidentDetails = Page.verifyOnPage(IncidentDetails)
     incidentDetailsPage.exitButton().click()
