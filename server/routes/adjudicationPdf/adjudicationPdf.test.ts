9f3ed056abb1d5b49c273711279e51e0ee30f615
Richard Popple <richard.popple@digital.justice.gov.uk>
2022-03-30 16:37:24 +0100

    NN-3816 adding test

---
+++ server/routes/adjudicationPdf/adjudicationPdf.test.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from 'express'
+
+import ReportedAdjudicationsService from '../../services/reportedAdjudicationsService'
+import NoticeOfBeingPlacedOnReportData from '../../data/noticeOfBeingPlacedOnReportData'
+import config from '../../config'
+import DecisionTreeService from '../../services/decisionTreeService'
+
+export default class AdjudicationPdf {
+  constructor(
+    private readonly reportedAdjudicationsService: ReportedAdjudicationsService,
+    private readonly decisionTreeService: DecisionTreeService
+  ) {}
+
+  renderPdf = async (req: Request, res: Response): Promise<void> => {
+    const adjudicationNumber = Number(req.params.adjudicationNumber)
+    const { user } = res.locals
+    const { pdfMargins, adjudicationsUrl } = config.apis.gotenberg
+    const adjudicationDetails = await this.reportedAdjudicationsService.getConfirmationDetails(adjudicationNumber, user)
+
+    const { reportedAdjudication, associatedPrisoner, prisoner } =
+      await this.decisionTreeService.reportedAdjudicationIncidentData(adjudicationNumber, user)
+    const offences = await this.decisionTreeService.getAdjudicationOffences(
+      reportedAdjudication.offenceDetails,
+      prisoner,
+      associatedPrisoner,
+      reportedAdjudication.incidentRole,
+      user
+    )
+    const noticeOfBeingPlacedOnReportData = new NoticeOfBeingPlacedOnReportData(
+      adjudicationNumber,
+      adjudicationDetails,
+      offences
+    )
+    res.renderPdf(
+      `pages/noticeOfBeingPlacedOnReport`,
+      { adjudicationsUrl, noticeOfBeingPlacedOnReportData },
+      `pages/noticeOfBeingPlacedOnReportHeader`,
+      {},
+      `pages/noticeOfBeingPlacedOnReportFooter`,
+      { adjudicationNumber },
+      {
+        filename: `adjudication-report-${adjudicationNumber}`,
+        pdfMargins,
+      }
+    )
+  }
+}
