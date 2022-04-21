8c26bfb0eaa1070f01a14779a07e285f3bfdd921
sp-watson <steve.watson@digital,justice.gov.uk>
2022-04-21 16:55:30 +0100

    NN-4021: Go to correct statement page for reported adjudication

--- integration_tests/mockApis/adjudications.ts
+++ integration_tests/mockApis/adjudications.ts
@@ -290,6 +290,27 @@ const stubGetOffenceRule = ({
     },
   })
 
+const stubSaveOffenceDetails = ({
+  adjudicationNumber,
+  response = {},
+}: {
+  adjudicationNumber: number
+  response: Record<string, unknown>
+}): SuperAgentRequest =>
+  stubFor({
+    request: {
+      method: 'PUT',
+      url: `/adjudications/draft-adjudications/${adjudicationNumber}/offence-details`,
+    },
+    response: {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+      },
+      jsonBody: response,
+    },
+  })
+
 const verifySaveOffenceDetails = ({
   adjudicationNumber,
   offenceDetails,
@@ -319,5 +340,6 @@ export default {
   stubGetAllReportedAdjudications,
   stubCreateDraftFromCompleteAdjudication,
   stubGetOffenceRule,
+  stubSaveOffenceDetails,
   verifySaveOffenceDetails,
 }
