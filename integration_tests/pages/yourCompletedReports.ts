b69ec9bdb7394fa7c365a59cadcb21c392ccd6c5
richardpopple <richard.popple@digital.justice.gov.uk>
2022-04-22 14:20:59 +0100

    NN-4000 all completed reports page (#218)
    * NN-4001 adding the filter to the ui - will need some more styling at some point
    
    * NN-4001 filter works in principle - might need to do some refactoring - and still need to hook it into the api
    
    * NN-4001 fix up failing test
    
    * NN-4001 still need to display the status - but working in principle
    
    * NN-4001 adding the status column
    
    * NN-4001 fixing styling
    
    * NN-4001 fixing some tests
    
    * NN-4001 fixing linting
    
    * NN-4001 fixing linting
    
    * NN-4001 hopefully fixing bug that broke some tests
    
    * NN-4001 linting
    
    * NN-4001 linting
    
    * NN-4001 recommenting in tests
    
    * NN-4001 removing empty file
    
    * NN-4001 improving test
    
    * NN-4001 neater comparison of equals for numbers and string
    
    * NN-4001 linting
    
    * NN-4001 adding test for your completed reports
    
    * NN-4001 fixing bug
    
    * NN-4001 PR comments
    
    * NN-4001 fixing type issue
    
    * NN-4001 linting
    
    * NN-4001 adding cypress test
    
    * NN-4001 more cypress tests
    
    * NN-4001 more consistent data
    
    * NN-4001 linting
    
    * NN-4001 linting
    
    * NN-4000 first cut
    
    * NN-4000 rationalising template
    
    * NN-4000 filter added and working
    
    * NN-4000 commonising code
    
    * NN-4000 making the reporting officer captialisation fit with the screens
    
    * NN-4000 fixing the clear link
    
    * NN-4000 linting
    
    * NN-4000 adding extra column
    
    * NN-4000 fixing redirect url
    
    * NN-4000 making the tests more constitent
    
    * NN-4000 making the tests more constitent
    
    * NN-4000 more tests and refactoring the validation of roles
    
    * NN-4000 linting
    
    * NN-4000 fixing tests
    
    * NN-4000 refactoring and another test
    
    * NN-4000 refactoring of tests
    
    * NN-4000 refactor to commonise code
    
    * NN-4000 cypress tests
    
    Co-authored-by: sp-watson <77974320+sp-watson@users.noreply.github.com>

--- integration_tests/pages/yourCompletedReports.ts
+++ integration_tests/pages/yourCompletedReports.ts
@@ -17,23 +17,4 @@ export default class YourCompletedReportsPage extends Page {
     this.paginationLinks().contains(new RegExp(`^${number.toString()}$`))
 
   paginationResults = (): PageElement => cy.get('.moj-pagination__results').first()
-
-  // In order to bypass the date picker we force the input to accept text and then press escape so the date picker
-  // disappears allowing us to interact with other fields.
-  private forceDateInput = (dateInput: PageElement, day: number, month: number, year: number): PageElement =>
-    dateInput
-      .clear({ force: true })
-      .type(`${`0${day}`.slice(-2)}/${`0${month}`.slice(-2)}/${year}{esc}`, { force: true })
-
-  // This bypasses the date picker and manually forces the date field.
-  forceFromDate = (day: number, month: number, year: number): PageElement =>
-    this.forceDateInput(cy.get('[data-qa="from-date"]'), day, month, year)
-
-  // This bypasses the date picker and manually forces the date field.
-  forceToDate = (day: number, month: number, year: number): PageElement =>
-    this.forceDateInput(cy.get('[data-qa="to-date"]'), day, month, year)
-
-  applyButton = () => cy.get('[data-qa="filter-apply"]')
-
-  selectStatus = () => cy.get('#status')
 }
