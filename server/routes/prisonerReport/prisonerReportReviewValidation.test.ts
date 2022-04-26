653dc2b12212d1f2583dde6700e67c83902b48b9
tim <wright.tim.j@gmail.com>
2022-04-26 17:02:00 +0100

    NN-4002 bug fixes

--- server/routes/prisonerReport/prisonerReportReviewValidation.test.ts
+++ server/routes/prisonerReport/prisonerReportReviewValidation.test.ts
@@ -15,41 +15,43 @@ describe('validateForm - prisoner review', () => {
   })
   describe('status', () => {
     it('missing status', () => {
-      expect(validateForm({ status: null, reason: '', details: '' })).toStrictEqual(toTest.errors.MISSING_STATUS)
+      expect(validateForm({ status: null, reason: '', details: '' })).toStrictEqual([toTest.errors.MISSING_STATUS])
     })
   })
   describe('reason', () => {
     it('missing rejected reason', () => {
-      expect(validateForm({ status: ReviewStatus.REJECTED, reason: '', details: '' })).toStrictEqual(
-        toTest.errors.MISSING_REJECT_REASON
-      )
+      expect(validateForm({ status: ReviewStatus.REJECTED, reason: '', details: '' })).toStrictEqual([
+        toTest.errors.MISSING_REJECT_REASON,
+        toTest.errors.MISSING_REJECT_DETAILS,
+      ])
     })
     it('missing returned reason', () => {
-      expect(validateForm({ status: ReviewStatus.RETURNED, reason: '', details: '' })).toStrictEqual(
-        toTest.errors.MISSING_RETURN_REASON
-      )
+      expect(validateForm({ status: ReviewStatus.RETURNED, reason: '', details: '' })).toStrictEqual([
+        toTest.errors.MISSING_RETURN_REASON,
+        toTest.errors.MISSING_RETURN_DETAILS,
+      ])
     })
   })
   describe('details', () => {
     it('missing rejected details', () => {
-      expect(validateForm({ status: ReviewStatus.REJECTED, reason: 'reason', details: '' })).toStrictEqual(
-        toTest.errors.MISSING_REJECT_DETAILS
-      )
+      expect(validateForm({ status: ReviewStatus.REJECTED, reason: 'reason', details: '' })).toStrictEqual([
+        toTest.errors.MISSING_REJECT_DETAILS,
+      ])
     })
     it('missing returned details', () => {
-      expect(validateForm({ status: ReviewStatus.RETURNED, reason: 'reason', details: '' })).toStrictEqual(
-        toTest.errors.MISSING_RETURN_DETAILS
-      )
+      expect(validateForm({ status: ReviewStatus.RETURNED, reason: 'reason', details: '' })).toStrictEqual([
+        toTest.errors.MISSING_RETURN_DETAILS,
+      ])
     })
     it('rejected details too large', () => {
       expect(
         validateForm({ status: ReviewStatus.REJECTED, reason: 'reason', details: wordLimitExceedingString })
-      ).toStrictEqual(toTest.errors.REJECT_DETAILS_WORD_COUNT_EXCEEDED)
+      ).toStrictEqual([toTest.errors.REJECT_DETAILS_WORD_COUNT_EXCEEDED])
     })
     it('returned details too large', () => {
       expect(
         validateForm({ status: ReviewStatus.RETURNED, reason: 'reason', details: wordLimitExceedingString })
-      ).toStrictEqual(toTest.errors.RETURN_DETAILS_WORD_COUNT_EXCEEDED)
+      ).toStrictEqual([toTest.errors.RETURN_DETAILS_WORD_COUNT_EXCEEDED])
     })
   })
 })
