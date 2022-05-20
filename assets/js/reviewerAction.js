273e53cc3e394ce609c6f52d494ddd9a3e712a5d
Natalie Clamp <natalie.clamp@digital.justice.gov.uk>
2022-05-20 16:29:56 +0100

    NN-4111: Rm jquery, update callback

--- assets/js/reviewerAction.js
+++ assets/js/reviewerAction.js
@@ -1,18 +1,18 @@
 $(function () {
-  const $form = $('form')
-  $form.on('submit', e => {
+  const form = document.querySelector('form')
+  form.addEventListener('submit', e => {
     e.preventDefault()
-    const $radio = document.querySelector('[class="govuk-radios__item"] :checked')
-    if ($radio) {
-      const $radioChosen = $radio.value
+    const radio = document.querySelector('[class="govuk-radios__item"] :checked')
+    if (radio) {
+      const radioChosen = radio.value
       const eventCallback = function () {
-        $form.trigger('submit')
+        form.submit()
       }
-      if ($radioChosen === 'accepted') {
+      if (radioChosen === 'accepted') {
         gtag('event', 'REVIEWER ACCEPTED', {
           event_callback: eventCallback,
         })
-      } else if ($radioChosen === 'returned') {
+      } else if (radioChosen === 'returned') {
         gtag('event', 'REVIEWER RETURNED', {
           return_reason: document.querySelector('#returnedReasonId').value,
           return_details: document.querySelector('#returnedDetails').value,
