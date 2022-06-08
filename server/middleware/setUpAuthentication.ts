e209c08632b334b99ff85847ddc95cd0379a5526
sp-watson <steve.watson@digital,justice.gov.uk>
2022-06-08 12:22:31 +0100

    NN-4210: Update passport to 0.6

--- server/middleware/setUpAuthentication.ts
+++ server/middleware/setUpAuthentication.ts
@@ -30,13 +30,15 @@ export default function setUpAuth(): Router {
 
   const authSignOutUrl = `${config.apis.hmppsAuth.externalUrl}/sign-out?client_id=${config.apis.hmppsAuth.apiClientId}&redirect_uri=${config.domain}`
 
-  router.use('/sign-out', (req, res) => {
+  router.use('/sign-out', (req, res, next) => {
     if (req.user) {
-      req.logout()
-      req.session.destroy(() => res.redirect(authSignOutUrl))
-      return
+      req.logout(err => {
+        if (err) return next(err)
+        return req.session.destroy(() => res.redirect(authSignOutUrl))
+      })
+    } else {
+      res.redirect(authSignOutUrl)
     }
-    res.redirect(authSignOutUrl)
   })
 
   router.use((req, res, next) => {
