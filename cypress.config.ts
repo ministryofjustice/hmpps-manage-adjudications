525f8adba463bdad2fe431d01977c604043bf9c1
Tim Wright <wright.tim.j@gmail.com>
2022-06-28 16:10:07 +0100

    NN-4211 cypress 10 (#288)
    * NN-4211 cypress 10
    
    * NN-4211 put artifacts back
    
    * NN-4211 delete old index

---
+++ cypress.config.ts
@@ -0,0 +1,81 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'cypress'
+
+import { resetStubs } from './integration_tests/mockApis/wiremock'
+import prisonApi from './integration_tests/mockApis/prisonApi'
+import curiousApi from './integration_tests/mockApis/curiousApi'
+import { CaseLoad } from './server/data/prisonApiClient'
+import auth from './integration_tests/mockApis/auth'
+import prisonerSearch from './integration_tests/mockApis/prisonerSearch'
+import adjudications from './integration_tests/mockApis/adjudications'
+import tokenVerification from './integration_tests/mockApis/tokenVerification'
+
+export default defineConfig({
+  chromeWebSecurity: false,
+  fixturesFolder: 'integration_tests/fixtures',
+  screenshotsFolder: 'integration_tests/screenshots',
+  videosFolder: 'integration_tests/videos',
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json',
+  },
+  videoUploadOnPasses: false,
+  taskTimeout: 60000,
+  viewportWidth: 1024,
+  viewportHeight: 768,
+  e2e: {
+    setupNodeEvents(on) {
+      on('task', {
+        reset: resetStubs,
+
+        getSignInUrl: auth.getSignInUrl,
+        stubSignIn: (caseLoads: CaseLoad[]) => Promise.all([auth.stubSignIn(), prisonApi.stubUserCaseloads(caseLoads)]),
+        stubGetUserFromUsername: auth.stubGetUserFromUsername,
+
+        stubSearch: prisonerSearch.stubSearch,
+        stubAuthUser: auth.stubUser,
+        stubUserRoles: auth.stubUserRoles,
+        stubGetUser: auth.stubGetUser,
+        stubGetUserFromNames: auth.stubGetUserFromNames,
+        stubGetEmail: auth.stubGetEmail,
+
+        stubTokenPing: status => tokenVerification.stubPing(status),
+        stubAuthPing: status => auth.stubPing(status),
+        stubPrisonerSearchPing: status => prisonerSearch.stubPing(status),
+        stubPrisonApiPing: status => prisonApi.stubPing(status),
+        stubAdjudicationsPing: status => adjudications.stubPing(status),
+        stubCuriousPing: status => curiousApi.stubPing(status),
+        stubGetPrisonerDetails: prisonApi.stubGetPrisonerDetails,
+        stubGetLocations: prisonApi.stubGetLocations,
+        stubGetLocation: prisonApi.stubGetLocation,
+        stubGetAgency: prisonApi.stubGetAgency,
+        stubGetSecondaryLanguages: prisonApi.stubGetSecondaryLanguages,
+        stubGetBatchPrisonerDetails: prisonApi.stubGetBatchPrisonerDetails,
+
+        stubStartNewDraftAdjudication: adjudications.stubStartNewDraftAdjudication,
+        stubPostDraftIncidentStatement: adjudications.stubPostDraftIncidentStatement,
+        stubPutDraftIncidentStatement: adjudications.stubPutDraftIncidentStatement,
+        stubGetDraftAdjudication: adjudications.stubGetDraftAdjudication,
+        stubSubmitCompleteDraftAdjudication: adjudications.stubSubmitCompleteDraftAdjudication,
+        stubEditDraftIncidentDetails: adjudications.stubEditDraftIncidentDetails,
+        stubUpdateDraftIncidentRole: adjudications.stubUpdateDraftIncidentRole,
+        stubGetReportedAdjudication: adjudications.stubGetReportedAdjudication,
+
+        stubGetLearnerProfile: curiousApi.stubGetLearnerProfile,
+        stubGetAllDraftAdjudicationsForUser: adjudications.stubGetAllDraftAdjudicationsForUser,
+        stubGetYourReportedAdjudications: adjudications.stubGetYourReportedAdjudications,
+        stubGetAllReportedAdjudications: adjudications.stubGetAllReportedAdjudications,
+        stubCreateDraftFromCompleteAdjudication: adjudications.stubCreateDraftFromCompleteAdjudication,
+        stubGetOffenceRule: adjudications.stubGetOffenceRule,
+        stubSaveOffenceDetails: adjudications.stubSaveOffenceDetails,
+        verifySaveOffenceDetails: adjudications.verifySaveOffenceDetails,
+        stubUpdateAdjudicationStatus: adjudications.stubUpdateAdjudicationStatus,
+        stubSaveYouthOffenderStatus: adjudications.stubSaveYouthOffenderStatus,
+      })
+    },
+
+    baseUrl: 'http://localhost:3007',
+    specPattern: 'integration_tests/integration/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'integration_tests/support/index.ts',
+  },
+})
