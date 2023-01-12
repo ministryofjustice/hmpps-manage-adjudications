392313c824d10382f0efc1b969af22240926b132
Natalie Clamp <natalie.clamp@digital.justice.gov.uk>
2023-01-12 14:26:14 +0000

    NN-4757: Updates to DIS 1/2

--- server/routes/adjudicationPdf/adjudicationPdf.ts
+++ server/routes/adjudicationPdf/adjudicationPdf.ts
@@ -37,7 +37,7 @@ export default class AdjudicationPdf {
       `pages/noticeOfBeingPlacedOnReport`,
       { adjudicationsUrl, noticeOfBeingPlacedOnReportData },
       `pages/noticeOfBeingPlacedOnReportHeader`,
-      {},
+      { adjudicationNumber },
       `pages/noticeOfBeingPlacedOnReportFooter`,
       { adjudicationNumber },
       {
