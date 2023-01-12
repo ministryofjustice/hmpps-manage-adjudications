84c7e3fedfc1d8f02dda91dbe69774741c38d96d
Nat Clamp <84783598+natclamp-moj@users.noreply.github.com>
2023-01-12 16:17:43 +0000

    NN-4757: Remove charge number from DIS 1/2 footer (#470)

--- server/routes/adjudicationPdf/adjudicationPdf.ts
+++ server/routes/adjudicationPdf/adjudicationPdf.ts
@@ -39,7 +39,7 @@ export default class AdjudicationPdf {
       `pages/noticeOfBeingPlacedOnReportHeader`,
       { adjudicationNumber },
       `pages/noticeOfBeingPlacedOnReportFooter`,
-      { adjudicationNumber },
+      {},
       {
         filename: `adjudication-report-${adjudicationNumber}`,
         pdfMargins,
