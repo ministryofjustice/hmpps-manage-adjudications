ca48e0572365099609de6226e1b1ab0f047fcd4a
Nat Clamp <84783598+natclamp-moj@users.noreply.github.com>
2023-01-30 15:19:41 +0000

    NN-4667 (#487)

---
+++ server/routes/hearingOutcome/referralConfirmation/hearingReferralConfirmation.test.ts
@@ -0,0 +1,31 @@
+import { Express } from 'express'
+import request from 'supertest'
+import appWithAllRoutes from '../../testutils/appSetup'
+import adjudicationUrls from '../../../utils/urlGenerator'
+import UserService from '../../../services/userService'
+
+jest.mock('../../../services/userService')
+
+const userService = new UserService(null) as jest.Mocked<UserService>
+
+let app: Express
+
+beforeEach(() => {
+  app = appWithAllRoutes({ production: false }, { userService }, {})
+  userService.getUserRoles.mockResolvedValue(['ADJUDICATIONS_REVIEWER'])
+})
+
+afterEach(() => {
+  jest.resetAllMocks()
+})
+
+describe('GET /report-has-been-referred', () => {
+  it('should load the `referral confirmation` page', () => {
+    return request(app)
+      .get(adjudicationUrls.hearingReferralConfirmation.urls.start(100))
+      .expect('Content-Type', /html/)
+      .expect(res => {
+        expect(res.text).toContain('Referral saved')
+      })
+  })
+})
---
+++ server/routes/hearingOutcome/referralConfirmation/hearingReferralConfirmation.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express'
+
+import UserService from '../../../services/userService'
+import adjudicationUrls from '../../../utils/urlGenerator'
+import { hasAnyRole } from '../../../utils/utils'
+
+export default class HearingReasonForReferralPage {
+  constructor(private readonly userService: UserService) {}
+
+  private renderView = async (req: Request, res: Response): Promise<void> => {
+    const adjudicationNumber = Number(req.params.adjudicationNumber)
+
+    return res.render(`pages/hearingOutcome/hearingReferralConfirmation.njk`, {
+      returnLinkUrl: adjudicationUrls.hearingDetails.urls.review(adjudicationNumber),
+    })
+  }
+
+  view = async (req: Request, res: Response): Promise<void> => {
+    const userRoles = await this.userService.getUserRoles(res.locals.user.token)
+    if (!hasAnyRole(['ADJUDICATIONS_REVIEWER'], userRoles)) {
+      return res.render('pages/notFound.njk', { url: req.headers.referer || adjudicationUrls.homepage.root })
+    }
+    return this.renderView(req, res)
+  }
+}
