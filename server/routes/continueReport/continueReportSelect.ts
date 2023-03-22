3d2f261a597ffce3a0f3f123f9eaa8b7ce6d76e0
Peter Nikitin <peter.nikitin@digital.justice.gov.uk>
2023-03-22 15:19:31 +0000

    NN-4742 remove unused code

--- server/routes/continueReport/continueReportSelect.ts
+++ server/routes/continueReport/continueReportSelect.ts
@@ -57,18 +57,4 @@ export default class ContinueReportSelectRoutes {
     }
     return res.redirect(adjudicationUrls.continueReport.urls.filter(uiFilter))
   }
-
-  delete = async (req: Request, res: Response): Promise<void> => {
-    const { id } = req.params
-    const { user } = res.locals
-
-    const idValue: number = parseInt(id as string, 10)
-    if (Number.isNaN(idValue)) {
-      throw new Error('No draft adjudication id provided')
-    }
-
-    await this.placeOnReportService.removeDraftAdjudication(idValue, user)
-
-    return res.redirect(adjudicationUrls.continueReport.root)
-  }
 }
