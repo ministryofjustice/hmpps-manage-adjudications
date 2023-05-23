f351c6b35fb03972c246e1f3677006d1c34fea84
Tim Wright <wright.tim.j@gmail.com>
2023-05-23 15:54:31 +0100

    Revert "NN-5152 add line back to see if traces works (#630)"
    This reverts commit b8aa779955dcaef65e4b7559119980fc8389ea0b.

--- server/utils/azureAppInsights.ts
+++ server/utils/azureAppInsights.ts
@@ -26,7 +26,6 @@ export function buildAppInsightsClient(
   if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
     defaultClient.context.tags['ai.cloud.role'] = overrideName || applicationName
     defaultClient.context.tags['ai.application.ver'] = buildNumber
-    defaultClient.addTelemetryProcessor(addUserDataToRequests)
     return defaultClient
   }
   return null
