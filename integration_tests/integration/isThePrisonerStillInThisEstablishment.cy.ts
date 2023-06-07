d94968c7054c1b26907e9bb7b9fc716332ab5aa8
Natalie Clamp <natalie.clamp@digital.justice.gov.uk>
2023-06-07 16:02:00 +0100

    NN-5182: Int tests

--- integration_tests/integration/isThePrisonerStillInThisEstablishment.cy.ts
+++ integration_tests/integration/isThePrisonerStillInThisEstablishment.cy.ts
@@ -16,5 +16,32 @@ context('Is the prisoner still in this establishment?', () => {
       IsThePrisonerStillInThisEstablishmentPage
     )
     isThePrisonerStillInThisEstablishmentPage.checkOnPage()
+    isThePrisonerStillInThisEstablishmentPage.radios().should('exist')
+    isThePrisonerStillInThisEstablishmentPage.submitButton().should('exist')
+  })
+  it('should should go to normal search page if yes is selected', () => {
+    cy.visit(adjudicationUrls.isPrisonerStillInEstablishment.urls.start())
+    const isThePrisonerStillInThisEstablishmentPage: IsThePrisonerStillInThisEstablishmentPage = Page.verifyOnPage(
+      IsThePrisonerStillInThisEstablishmentPage
+    )
+    isThePrisonerStillInThisEstablishmentPage.checkOnPage()
+    isThePrisonerStillInThisEstablishmentPage.radioYes().click()
+    isThePrisonerStillInThisEstablishmentPage.submitButton().click()
+    cy.location().should(loc => {
+      expect(loc.pathname).to.eq(adjudicationUrls.searchForPrisoner.root)
+    })
+  })
+  it('should should go to normal search page with transfer query if no is selected', () => {
+    cy.visit(adjudicationUrls.isPrisonerStillInEstablishment.urls.start())
+    const isThePrisonerStillInThisEstablishmentPage: IsThePrisonerStillInThisEstablishmentPage = Page.verifyOnPage(
+      IsThePrisonerStillInThisEstablishmentPage
+    )
+    isThePrisonerStillInThisEstablishmentPage.checkOnPage()
+    isThePrisonerStillInThisEstablishmentPage.radioNo().click()
+    isThePrisonerStillInThisEstablishmentPage.submitButton().click()
+    cy.location().should(loc => {
+      expect(loc.pathname).to.eq(adjudicationUrls.searchForPrisoner.root)
+      expect(loc.search).to.eq('?transfer=true')
+    })
   })
 })
--- integration_tests/pages/isThePrisonerStillInThisEstablishment.ts
+++ integration_tests/pages/isThePrisonerStillInThisEstablishment.ts
@@ -1,7 +1,15 @@
-import Page from './page'
+import Page, { PageElement } from './page'
 
 export default class IsThePrisonerStillInThisEstablishmentPage extends Page {
   constructor() {
     super('Is the prisoner still in this establishment?')
   }
+
+  radios = (): PageElement => cy.get('[data-qa="still-in-establishment-radio-buttons"]')
+
+  radioYes = (): PageElement => cy.get('#stillInEstablishment')
+
+  radioNo = (): PageElement => cy.get('#stillInEstablishment-2')
+
+  submitButton = (): PageElement => cy.get('[data-qa="still-in-establishment-submit"]')
 }
