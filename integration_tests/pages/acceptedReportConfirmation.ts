7494d8d25d81f175acf2024d314d7cd02b89359b
Nat Clamp <84783598+natclamp-moj@users.noreply.github.com>
2023-06-20 16:11:04 +0100

    NN-5222: Add new content (#676)

--- integration_tests/pages/acceptedReportConfirmation.ts
+++ integration_tests/pages/acceptedReportConfirmation.ts
@@ -13,9 +13,19 @@ export default class AcceptedReportConfirmationPage extends Page {
 
   p3 = (): PageElement => cy.get('[data-qa="para-3"]')
 
+  tp1 = (): PageElement => cy.get('[data-qa="transfer-para-1"]')
+
+  tp2 = (): PageElement => cy.get('[data-qa="transfer-para-2"]')
+
+  tp3 = (): PageElement => cy.get('[data-qa="transfer-para-3"]')
+
+  tp4 = (): PageElement => cy.get('[data-qa="transfer-para-4"]')
+
   scheduleHearingButton = (): PageElement => cy.get('[data-qa="schedule-hearing-button"]')
 
   viewReportLink = (): PageElement => cy.get('[data-qa="view-report-link"]')
 
   allCompletedReportsLink = (): PageElement => cy.get('[data-qa="all-completed-reports-link"]')
+
+  inTextReportsLink = (): PageElement => cy.get('[data-qa="para-reports-link"]')
 }
