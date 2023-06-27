16c82fc841d6bfecce5489df3d98c959e981e051
Natalie Clamp <natalie.clamp@digital.justice.gov.uk>
2023-06-27 14:28:34 +0100

    NN-5223: Validation

--- server/routes/damages/addDamagesValidation.ts
+++ server/routes/damages/addDamagesValidation.ts
@@ -14,11 +14,16 @@ const errors: { [key: string]: FormError } = {
     href: '#damageType',
     text: 'Select what needs to be done',
   },
+  WORD_COUNT_EXCEEDED: {
+    href: '#damageDescription',
+    text: 'Your details must be 4,000 characters or fewer',
+  },
 }
 
 export default function validateForm({ damageDescription, damageType }: addDamagesForm): FormError | null {
   if (!damageType) return errors.RADIO_OPTION_MISSING
   if (!damageDescription) return errors.MISSING_TEXT
+  if (damageDescription.length > 4000) return errors.WORD_COUNT_EXCEEDED
 
   return null
 }
