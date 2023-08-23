8ec904ef50ed9a6dd875b2756ea2cfed5605efd0
Natalie Clamp <natalie.clamp@digital.justice.gov.uk>
2023-08-23 14:28:22 +0100

    NN-5104: Unit tests

---
+++ server/routes/punishments/reasonForChange/reasonForChange.test.ts
@@ -0,0 +1,73 @@
+import { Express } from 'express'
+import request from 'supertest'
+import appWithAllRoutes from '../../testutils/appSetup'
+import adjudicationUrls from '../../../utils/urlGenerator'
+import UserService from '../../../services/userService'
+import PunishmentsService from '../../../services/punishmentsService'
+import { PunishmentReasonForChange } from '../../../data/PunishmentResult'
+
+jest.mock('../../../services/userService')
+jest.mock('../../../services/punishmentsService')
+
+const userService = new UserService(null, null) as jest.Mocked<UserService>
+const punishmentsService = new PunishmentsService(null, null) as jest.Mocked<PunishmentsService>
+
+let app: Express
+
+beforeEach(() => {
+  app = appWithAllRoutes({ production: false }, { userService, punishmentsService }, {})
+  userService.getUserRoles.mockResolvedValue(['ADJUDICATIONS_REVIEWER'])
+})
+
+afterEach(() => {
+  jest.resetAllMocks()
+})
+
+describe('GET', () => {
+  beforeEach(() => {
+    app = appWithAllRoutes({ production: false }, { userService, punishmentsService }, {})
+    userService.getUserRoles.mockResolvedValue(['NOT_REVIEWER'])
+  })
+  it('should load the `Page not found` page', () => {
+    return request(app)
+      .get(adjudicationUrls.reasonForChangePunishment.urls.start('100'))
+      .expect('Content-Type', /html/)
+      .expect(res => {
+        expect(res.text).toContain('Page not found')
+      })
+  })
+})
+
+describe('GET ', () => {
+  it('should load the page', () => {
+    return request(app)
+      .get(adjudicationUrls.reasonForChangePunishment.urls.start('100'))
+      .expect('Content-Type', /html/)
+      .expect(res => {
+        expect(res.text).toContain('What is the reason for this change?')
+      })
+  })
+})
+
+describe('POST', () => {
+  it('should successfully call the endpoint and redirect', () => {
+    return request(app)
+      .post(adjudicationUrls.reasonForChangePunishment.urls.start('100'))
+      .send({
+        reasonForChange: PunishmentReasonForChange.OTHER,
+        detailsOfChange: 'This is a reason for the change',
+      })
+      .expect(302)
+      .expect('Location', `${adjudicationUrls.checkPunishments.urls.submittedEdit('100')}?punishmentsChanged=true`)
+      .then(() =>
+        expect(punishmentsService.setReasonForChangePunishments).toHaveBeenCalledWith(
+          expect.anything(),
+          {
+            reasonForChange: PunishmentReasonForChange.OTHER,
+            detailsOfChange: 'This is a reason for the change',
+          },
+          '100'
+        )
+      )
+  })
+})
---
+++ server/routes/punishments/reasonForChange/reasonForChangeValidation.test.ts
@@ -0,0 +1,33 @@
+import { PunishmentReasonForChange } from '../../../data/PunishmentResult'
+import validateForm from './reasonForChangeValidation'
+
+describe('validateForm', () => {
+  it('Valid submit has no errors', () => {
+    expect(
+      validateForm({
+        reasonForChange: PunishmentReasonForChange.OTHER,
+        detailsOfChange: 'this is the reason',
+      })
+    ).toBeNull()
+  })
+  it('error if reason missing', () => {
+    expect(
+      validateForm({
+        detailsOfChange: 'this is the reason',
+      })
+    ).toEqual({
+      href: '#reasonForChange',
+      text: 'Select a reason',
+    })
+  })
+  it('error if details missing', () => {
+    expect(
+      validateForm({
+        reasonForChange: PunishmentReasonForChange.OTHER,
+      })
+    ).toEqual({
+      href: '#detailsOfChange',
+      text: 'Enter more details',
+    })
+  })
+})
