6cc022238721cdd552eecf9086aa718a7f4b1aa1
Neil Mendum <neil.mendum@digital.justice.gov.uk>
2023-09-13 12:01:51 +0100

    NN-5562: apply a consistent pattern to prisoner information in DIS forms (#817)
    * NN-5562: apply a consistent pattern to prisoner information in DIS forms
    
    * NN-5562: fix tests; revert accidental commit

--- server/routes/disPdf/dis6Pdf.test.ts
+++ server/routes/disPdf/dis6Pdf.test.ts
@@ -72,8 +72,8 @@ describe('GET /dis6', () => {
         adjudicationsUrl: 'http://host.docker.internal:3000',
         conductReportData: {
           chargeNumber: '1524493',
-          establishmentName: 'MDI',
           prisonerDisplayName: 'Smith, John',
+          prisonerLocationDescription: 'Moorland (HMP & YOI) - 5-2-A-050',
           prisonerNumber: 'H5123BY',
         },
       },
