0e60c0064767f046e75a5f4c76b44a9a9a1e9a79
Neil Mendum <neil.mendum@digital.justice.gov.uk>
2023-09-29 11:18:50 +0100

    NN-5563: small tweaks to PC page; int tests to cover switching PC pages

--- integration_tests/pages/dataInsights.ts
+++ integration_tests/pages/dataInsights.ts
@@ -1,4 +1,4 @@
-import Page from './page'
+import Page, { PageElement } from './page'
 
 export default class DataInsightsPage extends Page {
   constructor() {
@@ -12,4 +12,14 @@ export default class DataInsightsPage extends Page {
   checkLastModifiedDate(value: string): void {
     cy.get('p').should('contain', `Date updated: ${value}`)
   }
+
+  selectCharacteristic = () => cy.get('[data-qa="characteristic-selector"]')
+
+  showResultsButton = (): PageElement => cy.get('[data-qa="show-results-submit"]')
+
+  ageReadEvidenceLink = (): PageElement => cy.get('[data-qa="age-read-evidence-link"]')
+
+  learningDisabilityReadEvidenceLink = (): PageElement => cy.get('[data-qa="learning-disability-read-evidence-link"]')
+
+  maturityReadEvidenceLink = (): PageElement => cy.get('[data-qa="maturity-read-evidence-link"]')
 }
