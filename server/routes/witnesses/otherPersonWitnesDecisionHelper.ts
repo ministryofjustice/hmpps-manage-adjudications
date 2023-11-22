e2d34106626ac790a94acea73e8b2077af6e3f1f
Natalie Clamp <natalie.clamp@digital.justice.gov.uk>
2023-11-22 15:02:39 +0000

    NN-5718: Add validation check for two names for witness

--- server/routes/witnesses/otherPersonWitnesDecisionHelper.ts
+++ server/routes/witnesses/otherPersonWitnesDecisionHelper.ts
@@ -9,12 +9,22 @@ import { properCaseName } from '../../utils/utils'
 // eslint-disable-next-line no-shadow
 enum ErrorType {
   OTHER_PERSON_MISSING_NAME_INPUT_SUBMIT = 'OTHER_PERSON_MISSING_NAME_INPUT_SUBMIT',
+  OTHER_PERSON_SINGLE_NAME = 'OTHER_PERSON_SINGLE_NAME',
 }
 const error: { [key in ErrorType]: FormError } = {
   OTHER_PERSON_MISSING_NAME_INPUT_SUBMIT: {
     href: '#otherPersonNameInput',
     text: 'Enter the person’s name',
   },
+  OTHER_PERSON_SINGLE_NAME: {
+    href: '#otherPersonNameInput',
+    text: 'Enter the person’s first and last names',
+  },
+}
+
+const prepareWitnessName = (otherPersonNameInput: string) => {
+  const trimmedName = otherPersonNameInput.trim().replace(/ +/g, ' ')
+  return trimmedName.split(' ')
 }
 
 export default class OtherPersonWitnessDecisionHelper extends DecisionHelper {
@@ -38,19 +48,17 @@ export default class OtherPersonWitnessDecisionHelper extends DecisionHelper {
     if (!otherPersonData.otherPersonNameInput) {
       errors.push(error.OTHER_PERSON_MISSING_NAME_INPUT_SUBMIT)
     }
+    const names = prepareWitnessName(otherPersonData.otherPersonNameInput)
+    if (names.length < 2) {
+      errors.push(error.OTHER_PERSON_SINGLE_NAME)
+    }
     return errors
   }
 
   override async witnessNamesForSession(form: DecisionForm): Promise<unknown> {
     const { otherPersonNameInput } = form.selectedAnswerData as OtherPersonData
     if (otherPersonNameInput) {
-      const trimmedName = otherPersonNameInput.trim().replace(/ +/g, ' ')
-      const names = trimmedName.split(' ')
-      if (names.length < 2)
-        return {
-          firstName: null,
-          lastName: properCaseName(names[0]),
-        }
+      const names = prepareWitnessName(otherPersonNameInput)
       return {
         firstName: properCaseName(names[0]),
         lastName: properCaseName(names.reverse()[0]),
