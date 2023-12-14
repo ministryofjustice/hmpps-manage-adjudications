2fb776b4ba66698e98afc6c4eca08824c3f71e12
Nat Clamp <84783598+natclamp-moj@users.noreply.github.com>
2023-12-14 14:46:35 +0000

    NN-5756 (#941)

--- server/routes/createOnBehalfOf/createOnBehalfOfValidation.test.ts
+++ server/routes/createOnBehalfOf/createOnBehalfOfValidation.test.ts
@@ -11,4 +11,10 @@ describe('validateForm', () => {
       text: 'Enter the new reporting officer’s name',
     })
   })
+  it('shows error when officer name is too long', () => {
+    expect(validateForm('5AVKTfJ2YZ4sn3cQcukF8lw9VU8OWkngq')).toStrictEqual({
+      href: '#createdOnBehalfOfOfficer',
+      text: 'The reporting officer’s name must be less than 32 characters',
+    })
+  })
 })
