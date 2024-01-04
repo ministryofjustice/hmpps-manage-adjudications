e03cd7db3c2b890116b048a899db06342e065a17
Natalie Clamp <natalie.clamp@digital.justice.gov.uk>
2024-01-04 11:44:39 +0000

    NN-5772: Update govuk-frontend

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -63,8 +63,8 @@ export default function nunjucksSetup(app: express.Express, applicationInfo: App
   const njkEnv = nunjucks.configure(
     [
       path.join(__dirname, '../../server/views'),
-      'node_modules/govuk-frontend/',
-      'node_modules/govuk-frontend/components/',
+      'node_modules/govuk-frontend/dist/',
+      'node_modules/govuk-frontend/dist/govuk/components/',
       'node_modules/@ministryofjustice/frontend/',
       'node_modules/@ministryofjustice/frontend/moj/components/',
     ],
