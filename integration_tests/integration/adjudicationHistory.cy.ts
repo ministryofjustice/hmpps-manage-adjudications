97b5c91530c07d9762adcb5eaab2bc84b8fc267e
Nat Clamp <84783598+natclamp-moj@users.noreply.github.com>
2024-01-31 15:40:02 +0000

    NN-5841 (#979)
    * NN-5841
    
    * NN-5841: Remove .only

--- integration_tests/integration/adjudicationHistory.cy.ts
+++ integration_tests/integration/adjudicationHistory.cy.ts
@@ -4,6 +4,7 @@ import { formatDateForDatePicker, generateRange } from '../../server/utils/utils
 import { ReportedAdjudication, ReportedAdjudicationStatus } from '../../server/data/ReportedAdjudicationResult'
 import adjudicationUrls from '../../server/utils/urlGenerator'
 import TestData from '../../server/routes/testutils/testData'
+import { PunishmentType } from '../../server/data/PunishmentResult'
 
 const testData = new TestData()
 
@@ -38,7 +39,7 @@ context('Adjudication history', () => {
     adjudicationHistoryPage.paginationResults().should('exist')
   })
 
-  it('should display the results correctly', () => {
+  it('should display the results correctly - no punishments present', () => {
     const manyReportedAdjudications: ReportedAdjudication[] = generateRange(1, 5, _ => {
       return testData.reportedAdjudication({
         chargeNumber: _ as unknown as string,
@@ -53,6 +54,7 @@ context('Adjudication history', () => {
               'Destroys or damages any part of a young offender institution or any other property other than his own',
           },
         },
+        punishments: [],
       })
     })
     cy.task('stubGetPrisonerAdjudicationHistory', {
@@ -68,6 +70,53 @@ context('Adjudication history', () => {
     adjudicationHistoryPage.card().first().should('contain.text', 'Date of discovery: 15/11/2323 - 11:30')
     adjudicationHistoryPage.card().first().should('contain.text', 'Happened at: Moorland (HMP & YOI)')
     adjudicationHistoryPage.card().first().should('contain.text', 'Awaiting review')
+    adjudicationHistoryPage
+      .card()
+      .first()
+      .should(
+        'contain.text',
+        'Destroys or damages any part of a young offender institution or any other property other than his own'
+      )
+  })
+  it('should display the results correctly - punishments present', () => {
+    const manyReportedAdjudications: ReportedAdjudication[] = generateRange(1, 5, _ => {
+      return testData.reportedAdjudication({
+        chargeNumber: _ as unknown as string,
+        prisonerNumber: 'G6415GD',
+        dateTimeOfIncident: '2023-11-15T11:30:00',
+        dateTimeOfDiscovery: '2323-11-15T11:30:00',
+        offenceDetails: {
+          offenceCode: 17002,
+          offenceRule: {
+            paragraphNumber: '18',
+            paragraphDescription:
+              'Destroys or damages any part of a young offender institution or any other property other than his own',
+          },
+        },
+        punishments: [
+          testData.punishmentWithSchedule({}),
+          testData.punishmentWithSchedule({ type: PunishmentType.ADDITIONAL_DAYS, schedule: { days: 1 } }),
+        ],
+      })
+    })
+    cy.task('stubGetPrisonerAdjudicationHistory', {
+      bookingId: '123',
+      number: 0,
+      allContent: manyReportedAdjudications,
+    })
+
+    cy.visit(adjudicationUrls.adjudicationHistory.urls.start('G6415GD'))
+    const adjudicationHistoryPage: AdjudicationHistoryPage = Page.verifyOnPage(AdjudicationHistoryPage)
+    adjudicationHistoryPage.card().should('have.length', 5)
+    adjudicationHistoryPage.card().first().should('contain.text', '1')
+    adjudicationHistoryPage.card().first().should('contain.text', 'Date of discovery: 15/11/2323 - 11:30')
+    adjudicationHistoryPage.card().first().should('contain.text', 'Happened at: Moorland (HMP & YOI)')
+    adjudicationHistoryPage.card().first().should('contain.text', 'Awaiting review')
+    adjudicationHistoryPage
+      .card()
+      .first()
+      .should('contain.text', 'Loss of canteen: 10 days - suspended until 03/04/2023')
+    adjudicationHistoryPage.card().first().should('contain.text', 'Additional days: 1 day')
     adjudicationHistoryPage
       .card()
       .first()
