53b7a3de4376bf8a44fba1e693992bfb4469d1bf
Paul Massey <theDustRoom@users.noreply.github.com>
2025-01-15 14:04:02 +0000

    NN-6020 - PDF Rendering (#1229)
    * Replaced deprecated containerArgs
    
    From https://gotenberg.dev/docs/configuration#pdf-engines
    
    As of Gotenberg 8.13.0, the flag --pdfengines-engines has been deprecated.
    
    Updated gotenbergClient as per:
    https://github.com/ministryofjustice/hmpps-historical-prisoner/blob/main/server/data/gotenbergClient.ts
    
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>
    
    * Cleaned up code and simplified error handling
    
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>
    
    ---------
    
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>

--- server/utils/pdfRenderer.ts
+++ server/utils/pdfRenderer.ts
@@ -7,41 +7,37 @@ export type PdfFooterData = Record<string, unknown>
 
 export function pdfRenderer(client: GotenbergClient) {
   return (req: Request, res: Response, next: NextFunction) => {
-    res.renderPdf = (
-      pageView: string,
-      pageData: PdfPageData,
-      headerView: string,
-      headerData: PdfHeaderData,
-      footerView: string,
-      footerData: PdfFooterData,
+    res.renderPdf = async (
+      pageView,
+      pageData,
+      headerView,
+      headerData,
+      footerView,
+      footerData,
       options: { filename: string; pdfMargins: PdfMargins }
     ) => {
-      res.render(headerView, headerData, (headerError: Error, headerHtml: string) => {
-        if (headerError) {
-          throw headerError
-        }
-        res.render(footerView, footerData, (footerError: Error, footerHtml: string) => {
-          if (footerError) {
-            throw footerError
-          }
-          res.render(pageView, pageData, (bodyError: Error, pageHtml: string) => {
-            if (bodyError) {
-              throw bodyError
-            }
-
-            res.header('Content-Type', 'application/pdf')
-            res.header('Content-Transfer-Encoding', 'binary')
-            res.header('Content-Disposition', `inline; filename=${options.filename}`)
-
-            client
-              .renderPdfFromHtml(pageHtml, headerHtml, footerHtml, options?.pdfMargins)
-              .then(buffer => res.send(buffer))
-              .catch(error => {
-                res.status(500).send({ error: 'Failed to generate PDF' })
-              })
-          })
+      try {
+        const headerHtml = await new Promise<string>((resolve, reject) => {
+          res.render(headerView, headerData, (err, html) => (err ? reject(err) : resolve(html)))
         })
-      })
+
+        const footerHtml = await new Promise<string>((resolve, reject) => {
+          res.render(footerView, footerData, (err, html) => (err ? reject(err) : resolve(html)))
+        })
+
+        const pageHtml = await new Promise<string>((resolve, reject) => {
+          res.render(pageView, pageData, (err, html) => (err ? reject(err) : resolve(html)))
+        })
+
+        res.header('Content-Type', 'application/pdf')
+        res.header('Content-Transfer-Encoding', 'binary')
+        res.header('Content-Disposition', `inline; filename=${options.filename}`)
+
+        const buffer = await client.renderPdfFromHtml(pageHtml, headerHtml, footerHtml, options.pdfMargins)
+        res.send(buffer)
+      } catch (error) {
+        next(error)
+      }
     }
     next()
   }
