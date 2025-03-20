cdc3b94fe62aefedd7999f6e176a02a85a29918b
theDustRoom <paul.massey@digital.justice.gov.uk>
2025-03-20 17:15:53 +0000

    Enforce GlobalSearch rule
    Do not display prisoners who are “Out”
    
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>

--- server/routes/prisonerSearch/prisonerSearch.ts
+++ server/routes/prisonerSearch/prisonerSearch.ts
@@ -3,36 +3,50 @@ import { Request, Response } from 'express'
 import validateForm from './prisonerSearchValidation'
 import { FormError } from '../../@types/template'
 import adjudicationUrls from '../../utils/urlGenerator'
+import { hasAnyRole } from '../../utils/utils'
+import UserService from '../../services/userService'
 
 export default class PrisonerSearchRoutes {
-  private renderView = async (req: Request, res: Response, error?: FormError): Promise<void> => {
+  private userService: UserService
+
+  constructor(userService: UserService) {
+    this.userService = userService
+  }
+
+  private renderView = async (req: Request, res: Response, error?: FormError, globalError?: boolean): Promise<void> => {
     return res.render('pages/prisonerSearch', {
       errors: error ? [error] : [],
+      globalError,
     })
   }
 
-  view = async (req: Request, res: Response): Promise<void> => this.renderView(req, res)
+  view = async (req: Request, res: Response): Promise<void> => {
+    const transfer = req.query.transfer as string
+    if (transfer === 'true') {
+      const userRoles = await this.userService.getUserRoles(res.locals.user.token)
+      if (!hasAnyRole(['GLOBAL_SEARCH'], userRoles)) {
+        return this.renderView(req, res, null, true)
+      }
+    }
+    return this.renderView(req, res)
+  }
 
   submit = async (req: Request, res: Response): Promise<void> => {
     const { searchTerm } = req.body
     const transfer = req.query.transfer as string
-
     const error = validateForm({ searchTerm })
 
     if (error) return this.renderView(req, res, error)
 
+    const query: Record<string, string> = { searchTerm }
     if (transfer === 'true') {
-      return res.redirect(
-        url.format({
-          pathname: adjudicationUrls.selectPrisoner.root,
-          query: { searchTerm, transfer },
-        })
-      )
+      query.transfer = transfer
     }
+
     return res.redirect(
       url.format({
         pathname: adjudicationUrls.selectPrisoner.root,
-        query: { searchTerm },
+        query,
       })
     )
   }
