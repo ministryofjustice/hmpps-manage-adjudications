c31f5eefed06f929916187143b80a824d924adbe
Paul Massey <paul.massey@digital.justice.gov.uk>
2026-01-09 14:28:22 +0000

    Updated allow scripts
    Signed-off-by: Paul Massey <paul.massey@digital.justice.gov.uk>

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -11,6 +11,6 @@ export default configureAllowedScripts({
     // Needed by jest for running tests in watch mode
     'node_modules/fsevents@2.3.3': 'ALLOW',
     // Native solution to quickly resolve module paths, used by jest and eslint
-    'node_modules/unrs-resolver@1.8.1': 'ALLOW',
+    'node_modules/unrs-resolver@1.11.1': 'ALLOW',
   },
 })
